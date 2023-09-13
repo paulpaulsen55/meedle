cef1420d09d7c36b191343c5a2b3944320953f5c
Justus <justus.winter@urbanandmainlines.com>
2023-09-13 12:49:50 +0200

    removed animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,15 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-	import { tweened } from 'svelte/motion';
-	import { bounceOut, cubicOut } from 'svelte/easing';
 	import { goto } from '$app/navigation';
 	import { locations } from '../store';
 
-	let innerWidth = 0,
-		innerHeight = 0,
-		location1: string = 'tuxer steig 6',
+	let location1: string = 'tuxer steig 6',
 		location2: string = 'Rheinbabenallee 47';
 
-	const markerPosition = tweened({ x: 0, y: 0, r: 0, s: 1 });
-
-	let visible = false;
 	function startClick() {
-		visible = true;
 		locations.set({ location1, location2 });
-
-		markerPosition
-			.set({ x: 0, y: 0, r: 0, s: 1 })
-			.then(() =>
-				markerPosition.set(
-					{ x: 0, y: 100, r: 30, s: 1 },
-					{ duration: 400, easing: bounceOut, delay: 200 }
-				)
-			)
-			.then(() =>
-				markerPosition.set({ x: innerWidth / 4, y: 100, r: 10, s: 1.1 }, { duration: 300 })
-			)
-			.then(() =>
-				markerPosition.set({ x: innerWidth / 2 + 55, y: 100, r: -10, s: 1 }, { duration: 350 })
-			)
-			.then(() => goto('/map'));
+		goto('/map');
 	}
 </script>
-
-<svelte:window bind:innerWidth bind:innerHeight />
 
 <div class="h-screen grid place-items-center">
 	<img src="/logo.svg" alt="logo" />
@@ -55,33 +29,8 @@
 				<input bind:value={location2} type="text" id="location2" class="input" />
 			</div>
 		</div>
-		<button
-			on:click={() => startClick()}
-			class="button-magnum w-64 flex justify-center relative group"
-		>
-			{#if visible}
-				<svg
-					in:fly={{ x: 10, duration: 200, easing: cubicOut }}
-					xmlns="http://www.w3.org/2000/svg"
-					width="38"
-					height="25"
-					class="absolute left-10"
-					style={'transform: translate(' +
-						$markerPosition.x +
-						'px, ' +
-						$markerPosition.y +
-						'px) rotate(' +
-						$markerPosition.r +
-						'deg) scale(' +
-						$markerPosition.s +
-						')'}
-					><path
-						fill="#783A15"
-						d="M37.98 12.5c.006 0 .02-.014.02-.014S19.22 0 13.048 0C3.965 0 .014 6.328 0 12.5.014 18.672 3.965 25 13.048 25 19.22 25 38 12.507 38 12.507l-.02-.007Zm-25.448 4.326c-2.346 0-4.245-1.939-4.245-4.333 0-2.395 1.899-4.333 4.245-4.333 2.345 0 4.244 1.945 4.244 4.34 0 2.387-1.899 4.326-4.244 4.326Z"
-					/></svg
-				>
-			{/if}
-			<span class="">start</span>
+		<button on:click={() => startClick()} class="button-magnum w-64 flex justify-center">
+			start
 		</button>
 	</div>
 </div>
