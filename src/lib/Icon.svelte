e5defe7656ee1e548bf64eb146ffca43d77937c9
paulpaulsen55 <paulpaulsenx@gmail.com>
2023-09-18 15:45:07 +0200

    added new Icon component

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let name: keyof typeof icons;
+	let icons = {
+		edit: `<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />`,
+		arrowLeft: `<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />`,
+		settingsHorizontal: `<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />`
+	} as const;
+	let displayIcon = icons[name];
+</script>
+
+<svg
+	class={'w-6 h-6 ' + $$props.class}
+	stroke-width="1.5"
+	stroke="currentColor"
+	fill="none"
+	viewBox="0 0 24 24"
+>
+	{@html displayIcon}
+</svg>
