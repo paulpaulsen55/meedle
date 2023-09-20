b1c0827fd00603fbd42ee588e35e7e3ff778f0a6
Justus <justus.winter@urbanandmainlines.com>
2023-09-20 09:01:32 +0200

    fixed row design

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -3,6 +3,7 @@
     import { food } from '$lib/helpers/filters';
     import { activities } from '$lib/helpers/filters';
     import { shopping } from '$lib/helpers/filters';
+    import { X } from 'lucide-svelte';
    
     const {
     elements: { root, tag, deleteTrigger },
@@ -30,11 +31,7 @@
     });
   </script>
    
-  <button
-    use:melt={$trigger}
-    class="inline-flex items-center justify-center rounded-md bg-white px-4 py-2
-      font-medium leading-none text-magnum-700 shadow-lg hover:opacity-75"
-  >
+  <button use:melt={$trigger} class="button-white">
     Delete Item
   </button>
    
@@ -42,7 +39,7 @@
     {#if $open}
       <div use:melt={$overlay} class="fixed inset-0 z-50 bg-black/50" />
       <div
-        class="fixed left-[50%] top-[50%] z-50 max-h-[85vh] w-[90vw]
+        class="fixed left-1/2 top-1/2 z-50 max-h-[85vh] w-[90vw]
               max-w-[400px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-white
               p-6 shadow-lg"
         use:melt={$content}
@@ -50,17 +47,17 @@
       <h2 use:melt={$title} class="m-0 text-lg font-medium text-black">
         Essen und Trinken
       </h2>
-        <div class="grid grid-rows-2 grid-flow-col auto-cols-max gap-1 overflow-x-scroll">
-          {#each food.entries() as [k, _]}
-          <div>
+        <div class="whitespace-nowrap overflow-x-auto">
+          {#each food.entries() as [k, _], index}
+            {#if index == Math.floor(food.size / 2)}
+              <br class="inline-block">
+            {/if}
             <button
               on:click={() => {addTag(k)}}
-              class="inline-flex h-8 items-center justify-center rounded-[4px]
-                    bg-magnum-100 px-4 font-medium leading-none text-magnum-900"
-            >
-              {k}
-            </button>
-          </div>
+              class="inline-block h-8 rounded bg-magnum-100 px-4 m-0.5 font-medium text-magnum-900"
+              >
+                {k}
+              </button>
           {/each}
         </div>
         <h2 use:melt={$title} class="m-0 text-lg font-medium text-black">
@@ -114,8 +111,7 @@
               <div
                 use:melt={$tag(t)}
                 class="flex items-center overflow-hidden rounded-md bg-magnum-200 text-magnum-900 [word-break:break-word]
-              data-[disabled]:bg-magnum-300 data-[selected]:bg-magnum-400 data-[disabled]:hover:cursor-default
-                data-[disabled]:focus:!outline-none data-[disabled]:focus:!ring-0"
+             data-[selected]:bg-magnum-400"
               >
                 <span class="flex items-center border-r border-white/10 px-1.5"
                   >{t.value}</span
@@ -124,7 +120,7 @@
                   use:melt={$deleteTrigger(t)}
                   class="flex h-full items-center px-1 enabled:hover:bg-magnum-300"
                 >
-                x
+                <X />
                 </button>
               </div>
             {/each}
