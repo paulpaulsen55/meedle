c607fa9e490732dfe5414fd8c7bfa489196f2c6e
Konrad <87895872+Calliope2@users.noreply.github.com>
2023-09-26 15:00:46 +0200

    Add popover to set radius & number of POIs (#16)
    * Add popover to set radius & number of POIs
    
    * rebase main
    
    * fix design
    
    * add radius calculation and number of POIs to API-Call
    
    * add popover on map-subpage
    
    * rename POI to POIs and Radius to Radius (km)
    
    * fix design on map-subpage
    
    * Change button-icon to new SlidersHorizontal
    
    * Start reformating of button-icon
    
    * fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,21 @@
 	import { locations } from '../store';
 	import NavBar from '$lib/NavBar.svelte';
 	import AddressInput from '$lib/AddressInput.svelte';
+	import AdressSettings from '$lib/AdressSettings.svelte';
+	import { radius as r } from '../store';
+	import { poi as p } from '../store';
 
 	export let data
 
 	let location1: string = 'tuxer steig 6',
 		location2: string = 'Rheinbabenallee 47';
+	let radius = 5;
+	let poi = 5;
 
 	function startClick() {
 		locations.set({ location1, location2 });
+		r.set(radius);
+		p.set(poi);
 		goto('/map');
 	}
 </script>
@@ -24,8 +31,11 @@
 			<AddressInput bind:location={location1} sessionToken={data.sessionToken} />
 			<AddressInput bind:location={location2} sessionToken={data.sessionToken} />
 		</div>
-		<button on:click={() => startClick()} class="button-magnum w-64 mt-5 flex justify-center">
-			start
-		</button>
+		<div class="space-x-3 flex mt-5">
+			<AdressSettings bind:radius bind:poi class="button-magnum" />
+			<button on:click={() => startClick()} class="button-magnum w-80 justify-center">
+				meet me in the middle
+			</button>
+		</div>
 	</div>
 </div>
--- src/store.ts
+++ src/store.ts
@@ -2,5 +2,7 @@ import { writable } from 'svelte/store';
 
 export const locations = writable({
 	location1: '',
-	location2: ''
+	location2: '',
 });
+export const radius = writable(0)
+export const poi = writable(0)
