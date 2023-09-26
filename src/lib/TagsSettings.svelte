b899c9904e094487a7ac30d4793742195df94a15
Sander Hahn <sander.hahn@helmholtz-berlin.de>
2023-09-26 14:43:49 +0200

    added search functionality & formatting

--- src/lib/TagsSettings.svelte
+++ src/lib/TagsSettings.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-	import { createDialog, createTagsInput, melt, type CreateDialogProps, type Tag } from '@melt-ui/svelte';
+	import {
+		createDialog,
+		createTagsInput,
+		melt,
+		type CreateDialogProps,
+		type Tag
+	} from '@melt-ui/svelte';
 	import { filters } from '$lib/helpers/filters';
 	import { X, Plus } from 'lucide-svelte';
 	import Scroller from '$lib/Scroller.svelte';
 	import { createEventDispatcher } from 'svelte';
 
 	let filterTags: Array<Tag> = [],
-	  tempTags: Tag[] = [],
-    continueOption = false;
-  
-  let searchTerm = '';
-  let results: Map<string,string>[] = [];
+		tempTags: Tag[] = [],
+		continueOption = false;
+
+	let searchTerm = '';
+	let results: Map<string, string>[] = [];
 
 	const dispatch = createEventDispatcher();
 
-  const displayTagsInput = createTagsInput({ unique: true }),
-        displayTagElement = displayTagsInput.elements.tag,
-        displayTags = displayTagsInput.states.tags,
-        displayDeleteTrigger = displayTagsInput.elements.deleteTrigger;
+	const displayTagsInput = createTagsInput({ unique: true }),
+		displayTagElement = displayTagsInput.elements.tag,
+		displayTags = displayTagsInput.states.tags,
+		displayDeleteTrigger = displayTagsInput.elements.deleteTrigger;
 
 	const {
 		elements: { tag, deleteTrigger },
@@ -30,27 +36,27 @@
 		}
 	});
 
-  const handleOpen: CreateDialogProps['onOpenChange'] = ({ curr, next }) => {
-    if (open && !next) {
-      if (!continueOption) {
-        tags.set(tempTags);
-      }
-      return false // close
-    }
-    return next
-  }
+	const handleOpen: CreateDialogProps['onOpenChange'] = ({ curr, next }) => {
+		if (open && !next) {
+			if (!continueOption) {
+				tags.set(tempTags);
+			}
+			return false; // close
+		}
+		return next;
+	};
 
 	const {
 		elements: { trigger, title, content, portalled },
 		states: { open }
 	} = createDialog({
 		role: 'alertdialog',
-    onOpenChange: handleOpen
-  });
+		onOpenChange: handleOpen
+	});
 
 	function openClick() {
 		tempTags = filterTags.map((item) => ({ ...item }));
-    continueOption = false;
+		continueOption = false;
 	}
 
 	function closeClick() {
@@ -60,35 +66,35 @@
 
 	function continueClick() {
 		filterTags = $tags;
-    continueOption = true;
+		continueOption = true;
 		open.set(false);
-    displayTags.set(filterTags); // update displayed tags when filterTags changes
+		displayTags.set(filterTags); // update displayed tags when filterTags changes
 		dispatch('updateTags', filterTags); // send tags to parent
 	}
 
-  let sussyBussy = (map: Map<string, string>) => {
-    if (searchTerm.length == 0) {
-      return map;
-    }
-    let result = new Map();
-    map.forEach((key, data) => {
-      if (key.toLowerCase().includes(searchTerm.toLowerCase())) {
-        result.set(key, data);
-      }
-    });
-    return result;
-  }
+	let sussyBussy = (map: Map<string, string>) => {
+		if (searchTerm.length == 0) {
+			return map;
+		}
+		let result = new Map();
+		map.forEach((key, data) => {
+			if (data.toLowerCase().includes(searchTerm.toLowerCase())) {
+				result.set(data, key);
+			}
+		});
+		return result;
+	};
 
-  $ : if (searchTerm.length > 0  && filters) {
-    results = [];
-    let newFilters = new Map();
-    Object.entries(filters).forEach(([key, value]) => {
-      newFilters.set(key, sussyBussy(value));
-    });
-    results.push(newFilters);
-  } else if (searchTerm.length == 0  && filters){
-    results = Object.values(filters);
-  }
+	$: if (searchTerm.length > 0 && filters) {
+		results = [];
+		const r = Object.values(filters);
+		r.forEach((map) => {
+			results.push(sussyBussy(map));
+		});
+	} else if (searchTerm.length == 0 && filters) {
+		results = Object.values(filters);
+		console.log(results);
+	}
 </script>
 
 <div class="flex space-x-2 h-6">
@@ -101,7 +107,10 @@
 	</button>
 	<div class="flex gap-2">
 		{#each $displayTags as t}
-			<div use:melt={$displayTagElement(t)} class="flex rounded-md bg-magnum-300 text-magnum-900 py-2">
+			<div
+				use:melt={$displayTagElement(t)}
+				class="flex rounded-md bg-magnum-300 text-magnum-900 py-2"
+			>
 				<button use:melt={$displayDeleteTrigger(t)} class="flex items-center rounded m-0.5">
 					<span class="px-1">{t.value}</span>
 					<X class="h-5 w-5" />
@@ -113,34 +122,39 @@
 
 <div use:melt={$portalled}>
 	{#if $open}
-  <div class="fixed inset-0 z-10 backdrop-blur"
-    ></div>
+		<div class="fixed inset-0 z-10 backdrop-blur" />
 		<div
 			class="fixed left-1/2 top-1/2 z-50 max-h-[85vh] w-[90vw]
               max-w-[340px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-neutral-900
               p-6 shadow-lg"
 			use:melt={$content}
 		>
-
-			<input type="text" id="tag" placeholder="Enter Tag" class="input w-3/4" bind:value={searchTerm} />
+			<input
+				type="text"
+				id="tag"
+				placeholder="Enter Tag"
+				class="input w-3/4"
+				bind:value={searchTerm}
+			/>
 
 			{#each Object.entries(filters) as filter, i}
 				<h2 use:melt={$title} class="m-1 text-lg font-medium text-white">{filter[0]}</h2>
 				<Scroller style="height: 4.5rem">
 					{#each results[i].entries() as [k, _], j}
-              {#if j == Math.floor(results[i].size / 2)}
-                <br class="inline-block" />
-              {/if}
-              <button
-                on:click={() => { addTag(k); }}
-                class="inline-block h-8 rounded bg-magnum-200 px-4 m-0.5 font-medium text-magnum-900"
-              >
-                {k}
-              </button>
+						{#if j == Math.floor(results[i].size / 2)}
+							<br class="inline-block" />
+						{/if}
+						<button
+							on:click={() => {
+								addTag(k);
+							}}
+							class="inline-block h-8 rounded bg-magnum-200 px-4 m-0.5 font-medium text-magnum-900"
+						>
+							{k}
+						</button>
 					{/each}
 				</Scroller>
 			{/each}
-
 
 			<div class="my-5">
 				<Scroller arrowSize={5}>
@@ -167,7 +181,7 @@
 				<button
 					on:click={() => continueClick()}
 					class="inline-flex h-8 items-center justify-center rounded-[4px] bg-magnum-100 px-4 font-medium leading-none text-magnum-900"
-        >
+				>
 					Continue
 				</button>
 			</div>
