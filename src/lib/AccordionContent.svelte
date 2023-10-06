a6ccd10054f90ace98fb80cccea9f1814c2538c1
Justus <justus.winter@urbanandmainlines.com>
2023-10-06 16:58:20 +0200

    fixed tags overflow once and for all

--- src/lib/AccordionContent.svelte
+++ src/lib/AccordionContent.svelte
@@ -14,9 +14,9 @@
 </script>
 
 <div class="p-4 grid gap-4 place-items-center">
-	<div class="flex flex-nowrap w-[calc(100%-2rem)]">
+	<div class="flex gap-2 justify-center w-64">
 		{#each tags.length < 3 ? Array(tags.length) : Array(3) as _, i}
-			<p class="truncate text-neutral-500 mx-2" title={tags[i]}>#{tags[i]}</p>
+			<p class="truncate text-neutral-500" title={tags[i]}>#{tags[i]}</p>
 		{/each}
 	</div>
 	{#await getTravelTimes(from, feature.coordinate)}
@@ -42,7 +42,7 @@
 		<a
 			href="https://www.google.de/maps/search/?api=1&query={feature.name}, {feature.address}"
 			target="_blank"
-			class="w-[calc(100%-2rem)] bg-neutral-500 text-white relative text-base py-2 rounded-md text-center hover:bg-neutral-600 transition"
+			class="w-64 bg-neutral-500 text-white relative text-base py-2 rounded-md text-center hover:bg-neutral-600 transition"
 		>
 			<ExternalLink class="absolute top-0 left-5 h-full w-6 text-white" />
 			<span>Google Maps</span>
