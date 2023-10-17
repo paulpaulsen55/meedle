561c1bb83b8bcb5e5176d5b650005f7ea959ca51
paulpaulsen55 <paulpaulsenx@gmail.com>
2023-10-18 00:01:47 +0200

    added logic for url params

---
+++ src/routes/map/+page.ts
@@ -0,0 +1,41 @@
+import { tagsList } from '$lib/helpers/tagsList';
+import type { Tag } from '@melt-ui/svelte';
+
+export async function load({ url }) {
+    const radius = url.searchParams.get('r');
+    const poi = url.searchParams.get('p');
+    const location1 = url.searchParams.get('a1');
+    const location2 = url.searchParams.get('a2');
+    const tags = url.searchParams.get('t')?.split(',');
+
+    
+
+    if (radius != null && poi != null && location1 != null && location2 != null && tags != null){
+        // get tag values
+        let tempTags: Tag[] = [];
+        if (tags.length > 0){
+			for (let i = 0; i < tags.length; i++) {
+				let title = '';
+				Object.entries(tagsList).forEach(([k,m]) => {	
+                    const t = m.get(tags[i]);
+                    if (t){
+                        title = t;
+                    }
+				});
+				tempTags.push({id: tags[i], value: title});
+			}
+		}
+
+        // TODO: remove console.log
+        console.log(radius, poi, location1, location2, tempTags);
+
+        return {
+            radius: Number(radius),
+            poi: Number(poi),
+            location1: location1,
+            location2: location2,
+            tags: tempTags
+        } 
+    }
+    return;
+}  
