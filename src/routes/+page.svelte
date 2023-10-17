19d791034d07bbe5f8bca089c4c22c21c8c02f4d
Justus <justus.winter@urbanandmainlines.com>
2023-10-17 13:10:47 +0200

    fixed errors from merge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -212,8 +212,8 @@
 				Aus diesem Grund war es die einzig richtige Schlussfolgerung, Meedle zu gründen: Die perfekte Plattform, um deeinen nächsten Treffpunkt zu ermitteln.
 			</p>
 		</div>
-		<img src="/team.jpeg" alt="team" class="h-[auto] max-w-[450px]">
-		<img src="/software.jpeg" alt="entwicklung" class="h-[auto] max-w-[450px]">
+		<img src="/team.jpeg" alt="team" class="h-[auto] w-full">
+		<img src="/software.jpeg" alt="entwicklung" class="h-[auto] w-full">
 		<div>
 			<h2 class="text-2xl font-semibold mb-5 dark:text-black text-white">wer wir sind?</h2>
 			<p class="dark:text-black text-white">
