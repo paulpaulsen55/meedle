b82b07708509f20bd0d7d04b1a6ea21af92e3dc9
Johannes <Johannes.Foltin@gmx.de>
2023-10-29 14:05:03 +0100

    relative pushed

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -275,14 +275,14 @@
 </script>
 
 <div class="h-screen w-full md:w-[calc(100vw - 24rem)]" id="map" bind:this={mapElement}/>
-<div class="svg-container absolute bottom-16 right-2 z-0  " on:click={()=> changeZone = !changeZone}>
+<div class="svg-container md:absolute md:bottom-[4.5rem] relative bottom-40 right-2 z-0  " on:click={()=> changeZone = !changeZone}>
     {#if changeZone}
-        <CircleDotDashed class="h-8 w-8"/>
+        <CircleDotDashed class="h-8 w-8 dark:text-black"/>
     {:else}
-        <CircleDot class="h-8 w-8"/>
+        <CircleDot class="h-8 w-8 dark:text-black"/>
     {/if}
 </div>
-<button on:click={() => changeStyle()} class="absolute bottom-28 md:bottom-6 right-2 z-0">
+<button on:click={() => changeStyle()} class="md:absolute bottom-28 relative md:bottom-6 right-2 z-0">
     <ThemeSwitch/>
 </button>
 
