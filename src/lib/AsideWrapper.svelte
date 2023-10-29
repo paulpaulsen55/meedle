3311a671a71c0cffd2a20ec039096278d83f5c4c
Johannes <Johannes.Foltin@gmx.de>
2023-10-29 20:18:28 +0100

    added a new bar (look the same but now you can only move it with the bar)

--- src/lib/AsideWrapper.svelte
+++ src/lib/AsideWrapper.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import { ArrowLeftIcon } from "lucide-svelte";
-    import { onMount } from "svelte";
-    import { circInOut, linear } from "svelte/easing";
-    import { tweened } from "svelte/motion";
+    import {ArrowLeftIcon} from "lucide-svelte";
+    import {onMount} from "svelte";
+    import {circInOut, linear} from "svelte/easing";
+    import {tweened} from "svelte/motion";
 
     let isMobile = false;
     let width = 0, height = 0;
     let prevTouchY = 0;
     let moving = false;
-    let y = tweened(50, { duration: 150, easing: circInOut });
+    let y = tweened(50, {duration: 150, easing: circInOut});
     let positions = [50]
 
     export function setOpen(open: boolean) {
@@ -18,18 +18,14 @@
         else y.set(height - 100);
     }
 
-    onMount(()  => {
+    onMount(() => {
         // add the height of the aside to the positions array | onMount, because the height of the aside is not known before
         const min = height - 100;
         positions.push(min);
         y.set(min);
     })
 
-    $ : if (width <= 768) { // tailwind breakpoint md
-        isMobile = true;
-    } else {
-        isMobile = false;
-    }
+    $ : isMobile = width <= 768;
 
     function onDown() {
         if (!isMobile) return;
@@ -38,14 +34,14 @@
 
     function onMove(e: MouseEvent | TouchEvent) {
         if (!isMobile) return;
-        if(e instanceof MouseEvent) {
+        if (e instanceof MouseEvent) {
             if (moving && $y + e.movementY >= 50 && $y + e.movementY <= height - 100) {
-                y.set($y + e.movementY, { duration: 0 , easing: linear});
+                y.set($y + e.movementY, {duration: 0, easing: linear});
             }
-        } else if (e instanceof TouchEvent){
+        } else if (e instanceof TouchEvent) {
             const t = e.touches[0].pageY - prevTouchY;
             if (moving && $y + t >= 50 && $y + t <= height - 100) {
-                y.set($y + t, { duration: 0 , easing: linear});
+                y.set($y + t, {duration: 0, easing: linear});
             }
             prevTouchY = e.touches[0].pageY;
         }
@@ -60,20 +56,32 @@
     }
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} on:mouseup={onUp} on:mousemove={onMove} on:touchmove={onMove} on:touchend={onUp} />
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} on:mouseup={onUp} on:mousemove={onMove}
+               on:touchmove={onMove} on:touchend={onUp}/>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<aside
-        on:mousedown={() => onDown()}
-        on:touchstart={() => onDown()}
-        class={isMobile ? 'absolute z-10 flex justify-center w-full backdrop-blur' : ''}
-        style="top: {$y}px;"
->
-    <div class="flex flex-col p-6 h-screen w-96 dark:bg-slate-100 bg-neutral-900 rounded-md">
-        <a href="/" class="hidden md:inline-flex dark:text-black w-fit">
-            <ArrowLeftIcon class="w-12 h-12" />
-        </a>
-        <div class="inline-flex md:hidden w-1/2 self-center h-1 bg-neutral-500 rounded-md" />
-        <slot />
-    </div>
-</aside>
+{#if (isMobile)}
+    <aside
+            class='absolute z-10 flex flex-col justify-center items-center w-full backdrop-blur'
+            style="top: {$y}px;">
+            <div class="flex justify-center w-96 h-6 dark:bg-slate-100 bg-neutral-900 rounded-t-md"
+                 on:mousedown={() => onDown()}
+                 on:touchstart={() => onDown()}>
+                <div class="flex w-1/2 self-center h-1 bg-neutral-500 rounded-lg"/>
+            </div>
+            <div class="flex flex-col h-screen  w-96 dark:bg-slate-100 bg-neutral-900 px-6">
+                <slot/>
+            </div>
+    </aside>
+{:else}
+    <aside
+            style="top: {$y}px;"
+    >
+        <div class="flex flex-col h-screen w-96 dark:bg-slate-100 bg-neutral-900 rounded-md p-6">
+            <a href="/" class="hidden md:inline-flex dark:text-black w-fit">
+                <ArrowLeftIcon class="w-12 h-12"/>
+            </a>
+            <slot/>
+        </div>
+    </aside>
+{/if}
