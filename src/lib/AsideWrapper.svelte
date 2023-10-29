5f35a9ce7c549fd7e9b3dd0aa74492e23163f220
Johannes <Johannes.Foltin@gmx.de>
2023-10-29 22:55:37 +0100

    maybe Fixed?

--- src/lib/AsideWrapper.svelte
+++ src/lib/AsideWrapper.svelte
@@ -4,12 +4,15 @@
     import {circInOut, linear} from "svelte/easing";
     import {tweened} from "svelte/motion";
 
-    let isMobile = false;
-    let width = 0, height = 0;
-    let prevTouchY = 0;
-    let moving = false;
-    let y = tweened(50, {duration: 150, easing: circInOut});
-    let positions = [50]
+    let isMobile = false,
+        width = 0,
+        height = 0,
+        prevTouchY = 0,
+        moving = false,
+        y = tweened(50, {duration: 150, easing: circInOut}),
+        topOffset = 100,
+        bottomOffset = 50;
+
 
     export function setOpen(open: boolean) {
         if (!isMobile) return;
@@ -20,10 +23,11 @@
 
     onMount(() => {
         // add the height of the aside to the positions array | onMount, because the height of the aside is not known before
-        const min = height - 100;
-        positions.push(min);
-        y.set(min);
+        topOffset =  height - 100;
+        y.set(bottomOffset);
     })
+
+    $:console.log(height);
 
     $ : isMobile = width <= 768;
 
@@ -50,10 +54,14 @@
     function onUp() {
         if (!isMobile) return;
         moving = false;
-        y.set(positions.reduce((prev, curr) => {
-            return (Math.abs(curr - $y) < Math.abs(prev - $y) ? curr : prev);
-        }));
+
+        if (Math.abs(bottomOffset - $y) < Math.abs(topOffset - $y)){
+            y.set(bottomOffset);
+        }else {
+            y.set(topOffset);
+        }
     }
+
 </script>
 
 <svelte:window bind:innerWidth={width} bind:innerHeight={height} on:mouseup={onUp} on:mousemove={onMove}
